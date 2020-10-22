805b5426122625b925879496aee6032180bc6220
Henrique Leandro <miniqui10@gmail.com>
2020-10-22 03:47:32 -0200

    Formulário React finalizado

--- filmes/src/components/jumbotron/index.js
+++ filmes/src/components/jumbotron/index.js
@@ -3,10 +3,10 @@ import React, {Component} from 'react';
 export default class Jumbotron extends Component{
      render() {
          return (
-            <div class="jumbotron jumbotron-fluid">
-                <div class="container">
-                <h1 class="display-4">{this.props.titulo}</h1>
-                <p class="lead">{this.props.descricao}</p>
+            <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                <h1 className="display-4">{this.props.titulo}</h1>
+                <p className="lead">{this.props.descricao}</p>
 
                 </div>
                 
